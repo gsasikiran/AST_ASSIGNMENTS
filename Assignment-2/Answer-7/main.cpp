364b5d73a37cc8ae35fa246e2c036802857f79e5
Sasi Kiran <gsasikiran000@gmail.com>
2018-11-19 12:40:18 +0100

    Development code for answer 7

--- Assignment-2/Answer-7/main.cpp
+++ Assignment-2/Answer-7/main.cpp
@@ -4,22 +4,23 @@
 using namespace std;
 
 
+
 int main()
 {
 
     equation_solver solver;
 
     solver.solve_equation(10,20);
-    solver.get_result();
+    cout<<solver.get_result()<<endl;
 
-    solver.solve_equation(100,120);
-    solver.get_result();
+//    solver.solve_equation(100,120);
+//    solver.get_result();
 
-    solver.solve_equation(1000,1200);
-    solver.get_result();
+//    solver.solve_equation(1000,1200);
+//    solver.get_result();
 
-    solver.solve_equation(10000,10200);
-    solver.get_result();
+//    solver.solve_equation(10000,10200);
+//    solver.get_result();
 
 
     return 0;
--- Assignment-2/Answer-7/solver.cpp
+++ Assignment-2/Answer-7/solver.cpp
@@ -13,6 +13,19 @@ equation_solver::~equation_solver()
 
 bool equation_solver::solve_equation(const int k , const int n )
 {
+    int i;
+    double a;
+    double s;
+    s = (o_1)/(1-o_1);
+    for (i=2; i<k; i++)
+    {
+        s = (o_i_lesser/(1-o_i_lesser))*s;
+    }
+    for (i=k; k<n; i++)
+    {
+        s = (o_i_greater/(1-o_i_greater))*s;
+    }
+    this->result = s;
     return true;
 }
 
